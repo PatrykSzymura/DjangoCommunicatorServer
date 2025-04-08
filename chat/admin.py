01df33b2475cf633014acbeb16a3d3c04bb69c55
PatrykSzymura <patryk.ethan@gmail.com>
2025-04-08 18:50:13 +0200

    Adding Models to database

--- chat/admin.py
+++ chat/admin.py
@@ -5,3 +5,5 @@ from django.contrib import admin
 from chat import models as m
 
 admin.site.register(m.ChatUser)
+admin.site.register(m.Channel)
+admin.site.register(m.Messages)
--- chat/models.py
+++ chat/models.py
@@ -7,11 +7,13 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 import datetime
 from django.contrib.auth.models import User, Permission
 
+def get_deleted_user():
+    return get_user_model().objects.get_or_create(username='deleted-user')[0]
 
 #user profile
 class ChatUser(models.Model):
@@ -25,3 +27,36 @@ class ChatUser(models.Model):
 
     class Meta:
         verbose_name_plural = "Chat Users"
+
+class Channel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30,default="")
+    description = models.TextField(max_length=255, default="")
+
+    def __str__(self):
+        return self.name
+
+class ChannelMembers(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(ChatUser,on_delete=models.CASCADE)
+
+class Messages(models.Model):
+    id = models.AutoField(primary_key=True)
+    channelId = models.ForeignKey(Channel,on_delete=models.CASCADE)
+    userId = models.ForeignKey(ChatUser,on_delete=models.SET(get_deleted_user))
+    date = models.DateTimeField(auto_now_add=True)
+    was_edited = models.BooleanField(default=False)
+    edit_date = models.DateTimeField(auto_now=True)
+    message = models.TextField(max_length=500)
+
+    def __str__(self):
+        author = ChatUser.objects.get(id=self.userId.id)
+        log = str(self.date) + " Send by " + str(author) + " on "+ str(self.channelId) + "\n " + self.message
+
+        if self.was_edited:
+            log += "\n Was Edited on " + str(self.edit_date)
+        return log
+
+    class Meta:
+        verbose_name_plural = "Messages"
+
