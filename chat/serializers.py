3a18c066a97a2127f0b4b722250d961b05995493
PatrykSzymura <patryk.ethan@gmail.com>
2025-04-09 14:35:11 +0200

    .

--- chat/serializers.py
+++ chat/serializers.py
@@ -13,6 +13,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
 
         requested_chatUser_data = m.ChatUser.objects.get(user=user)
 
