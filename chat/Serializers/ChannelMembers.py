93f24c99251794df291f3b9843fd0bb8d510ce2b
PatrykSzymura <patryk.ethan@gmail.com>
2025-05-21 02:05:27 +0200

    .

--- chat/Serializers/ChannelMembers.py
+++ chat/Serializers/ChannelMembers.py
@@ -2,14 +2,16 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from chat import models as m
+from chat.Serializers.Channel import ChannelSerializer
 from chat.Serializers.User import NicknameSerializer
 
 
 class Serializer(serializers.Serializer):
     userId = NicknameSerializer()
+    channelId = ChannelSerializer()
     class Meta:
         model = m.ChannelMembers
-        fields = '__all__'
+        fields = ['channelId, userId']
 
     def create(self, validated_data):
         return m.ChannelMembers.objects.create(**validated_data)
