7cb717ac5b9a42950af70aaada107f2be5c82226
PatrykSzymura <patryk.ethan@gmail.com>
2025-06-09 21:48:28 +0200

    x

--- chat/urls.py
+++ chat/urls.py
@@ -15,17 +15,17 @@ urlpatterns = [
     path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', UserViews.CreateUser.as_view(), name='register_user'),
-    path('user/resetpassword/<int:pk>', UserViews.ChangePassword.as_view(), name='password_reset'),
-    path('user/profile/<int:pk>',UserViews.UpdateUser.as_view(), name='user_profile'),
-    path('user/delete/<int:pk>', UserViews.DeleteUser.as_view(), name='delete_user'),
+    path('user/resetpassword/<int:pk>/', UserViews.ChangePassword.as_view(), name='password_reset'),
+    path('user/profile/<int:pk>/',UserViews.UpdateUser.as_view(), name='user_profile'),
+    path('user/delete/<int:pk>/', UserViews.DeleteUser.as_view(), name='delete_user'),
 
-    path('user/get/current/<int:pk>', UserViews.RestrictedGetUserData.as_view(), name='get_current_user'),
-    path('user/get/list', UserViews.UserList.as_view(), name='get_user_list'),
+    path('user/get/current/<int:pk>/', UserViews.RestrictedGetUserData.as_view(), name='get_current_user'),
+    path('user/get/list/', UserViews.UserList.as_view(), name='get_user_list'),
 
     # CHAT-CHANNELS ENDPOINTS
     path('channel/create/new/', Channel.AddNew.as_view(), name='create_chat_channel'),
-    path('channel/get/list/brief', Channel.AllBrief.as_view() , name='get_chat_channels'),
-    path('channel/get/list/detail', Channel.AllDetail.as_view() , name='get_chat_channels'),
+    path('channel/get/list/brief/', Channel.AllBrief.as_view() , name='get_chat_channels'),
+    path('channel/get/list/detail/', Channel.AllDetail.as_view() , name='get_chat_channels'),
     path('channel/update/<int:pk>/', Channel.Update.as_view(), name='update_chat_channel'),
     path('channel/delete/<int:pk>/', Channel.Delete.as_view(), name='delete_chat_channel'),
 
@@ -36,12 +36,12 @@ urlpatterns = [
     path('channel/members/delete/', ChannelMembers.DeleteMember.as_view(), name='delete_chat_channel_member'),
 
     #MESSAGES management ENDPOINTS
-    path('messages/get/all/<int:pk>', v.MessagesView.as_view(), name='get_all_messages_for_chat'),
+    path('messages/get/all/<int:pk>/', v.MessagesView.as_view(), name='get_all_messages_for_chat'),
     path('messages/post/', v.MessagesCreateView.as_view(), name='post_messege'),
-    path('messages/edit/<int:pk>', v.MessagesUpdateView.as_view(), name='edit_messege'),
+    path('messages/edit/<int:pk>/', v.MessagesUpdateView.as_view(), name='edit_messege'),
     #path('messages/delete/<int:pk>', v.MessagesViewSet.as_view(), name='delete_messege'),
 
-    path('test/<int:pk>', UserViews.CreateUser.as_view()),
+    #path('test/<int:pk>', UserViews.CreateUser.as_view()),
 
 ]
 urlpatterns += websocket_urlpatterns
